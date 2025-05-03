0ef2f9e079e72c2ea04ad31b3db9da8cf781a969
MitaliM13 <mitalimaurya@kccemsr.edu.in>
2025-05-03 14:37:06 +0530

    Create folder structure

---
+++ src/app/auth/login/page.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function LoginPage() {
+    const [email, setEmail]  = useState('');
+    const [password, setPassword] = useState('')
+
+    
+}
